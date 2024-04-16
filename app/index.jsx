c263f4b479515fd3309768b32673174447aed396
Pranav R <pranav81234@gmail.com>
2024-04-16 23:28:44 +0530

    folder structure complete

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import {Link} from 'expo-router'
 
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text>Aora</Text>
+      <Text className='font-pblack '>Aora</Text>
+      <Link href='/home'>Go to Home</Link>
       <StatusBar style="auto" />
     </View>
   );
